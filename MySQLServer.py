3525ce5e658ea040d2da4b224a299953ee7ea890
mohamed231141758 <mohamed231141758@gmail.com>
2026-02-15 17:18:11 -0800

    Wrap database creation in function

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,24 +1,26 @@
 import mysql.connector
 from mysql.connector import Error
 
-connection = None
+def create_database():
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password=""
+        )
 
-try:
-    connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password=""
-    )
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+            cursor.close()
 
-    if connection.is_connected():
-        cursor = connection.cursor()
-        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-        print("Database 'alx_book_store' created successfully!")
-        cursor.close()
+    except Error as e:
+        print(f"Error: Unable to connect to MySQL server. {e}")
 
-except Error as e:
-    print(f"Error: Unable to connect to MySQL server. {e}")
+    finally:
+        if connection is not None and connection.is_connected():
+            connection.close()
 
-finally:
-    if connection is not None and connection.is_connected():
-        connection.close()
+create_database()
